13c54e264582308ef488e296218843baa949bdc5
Bruno Alves <bruno.santos@ifpi.edu.br>
2020-12-07 13:05:02 -0300

    new fonts

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -57,9 +57,9 @@ body {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
-  /* font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif; */
-  font-family: 'Poppins', sans-serif;
+  font-family: Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  /* font-family: 'Poppins', sans-serif; */
 
 }
 
